2ec48c0f79e31393dc393f7990754cf92055012b
cherif fourat <92321858+Cheriffouurat@users.noreply.github.com>
2023-03-07 16:32:02 +0100

    le 07/03

--- src/main/java/com/example/marketplace/repository/IRatingRepo.java
+++ src/main/java/com/example/marketplace/repository/IRatingRepo.java
@@ -1,7 +1,10 @@
 package com.example.marketplace.repository;
 
 import com.example.marketplace.entities.Rating;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IRatingRepo extends CrudRepository<Rating,Integer> {
+    @Query("select AVG(cr.note) from Product c inner join c.ratings cr where c.id= :id")
+    float NbRating(Integer id);
 }
