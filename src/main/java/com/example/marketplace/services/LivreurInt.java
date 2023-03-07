900fd973adb4516fa38e05c4a1df19f2a35913a5
Sabrinegharbii <gharbi.sabrine@esprit.tn>
2023-03-07 16:33:56 +0100

    livraisonle07/03

--- src/main/java/com/example/marketplace/services/LivreurInt.java
+++ src/main/java/com/example/marketplace/services/LivreurInt.java
@@ -2,7 +2,9 @@ package com.example.marketplace.services;
 
 
 import com.example.marketplace.entities.Livreur;
+import com.google.maps.errors.ApiException;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
@@ -13,5 +15,17 @@ public interface LivreurInt extends Serializable {
     Livreur updateLivreur(Long idlivreur, Livreur livreurDetails);
     public void deletelivreur(Long idlivreur);
     public List<Livreur> getLivreurDispo();
+   // public int countLivraison(Long idlivreur) ;
+   public int nbredelivraison(Long idLivreur);
 
-}
+    public int getdatedembauche(Long idLivreur) ;
+    public void ajouterunbonus(Long idLivreur);
+
+
+        ////////////////////
+    public double calculateDistance(String origin, String destination) throws ApiException, InterruptedException, IOException ;
+
+    public void retournestatut(Long idlivreur);
+    public void ajouterunbonusencasdelivadd(Long idLivreur);
+
+    }
